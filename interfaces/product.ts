824f623bcaabc9748e67e8c10e717962f36987e9
Anya Kovalieva <annakovalieva@gmail.com>
2022-04-11 19:48:29 +0200

    interfaces added

---
+++ interfaces/product.ts
@@ -0,0 +1,11 @@
+export default interface Product {
+    id: number
+    article_number: number,
+    name: string,
+    description: string,
+    specifiers: string,
+    stock: number,
+    location: string,
+    price: number,
+    api_key: string,
+};
