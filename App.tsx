cd156ac2f079222be9838a59151d58ffa0800062
Anya Kovalieva <annakovalieva@gmail.com>
2022-04-11 19:49:01 +0200

    added navigationContainer

--- App.tsx
+++ App.tsx
@@ -1,52 +1,48 @@
-// 04cacdd98a7db492190217400f297ab0
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import tea from './assets/tea.jpeg';
-import Stock from './components/Stock.tsx';
+import Home from "./components/Home";
+import Pick from "./components/Pick";
+import { useState } from 'react';
+import { AntDesign } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Base, Typography } from './styles';
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const routeIcons = {
+    "Stock": "home",
+    "Orders": "creditcard",
+  };
+  const [products, setProducts] = useState([]);
   return (
-    <SafeAreaView style={styles.container}>
-      {/* <View style={styles.base}> */}
-      <Image source={tea} style={styles.image} />
-        <View style={styles.titleOverlay}>
-          <Text style={styles.titleText}>
-            Lager-Appen</Text>
-        </View>
-      <ScrollView style={styles.scrollView}>
-        <Stock />
-      </ScrollView>
+    <SafeAreaView style={Base.container}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName = routeIcons[route.name] || "alert";
+
+              return <AntDesign name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'green',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen name="Stock">
+            {() => <Home products={products} setProducts={setProducts}
+            />}
+          </Tab.Screen>
+          {/* <Tab.Screen name="Stock" component={Home} /> */}
+          <Tab.Screen name="Orders">
+            {() => <Pick setProducts={setProducts} />}
+          </Tab.Screen>
+        </Tab.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e9f5db',
-  },
-  base: {
-    flex: 1,
-    paddingLeft: 12,
-    paddingRight: 12,
-  },
-  scrollView: {
-    marginHorizontal: 15,
-  },
-  titleOverlay: {
-    position: 'absolute',
-    top: 40,
-    left: "8%",
-  },
-  titleText: {
-    color: 'white',
-    fontSize: 50,
-  },
-  image: {
-    width: '100%',
-    height: '17%',
-    marginBottom: 20,
-  }
-});
