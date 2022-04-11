b16ed0e233c30a7200a0597bc32874006e6c5efb
Anya Kovalieva <annakovalieva@gmail.com>
2022-04-11 19:48:38 +0200

    models added

---
+++ models/orders.ts
@@ -0,0 +1,64 @@
+import config from "../config/config.json";
+import products from "./products";
+import Order from '../interfaces/order';
+import OrderItem from '../interfaces/orderItem';
+
+const orders = {
+    getOrders: async function getOrders(): Promise<Order[]> {
+        const response = await fetch(`${config.base_url}/orders?api_key=${config.api_key}`);
+        const result = await response.json();
+        return result.data;
+    },
+    pickOrder: async function pickOrder(order: Partial<Order>) {
+        // TODO: Minska lagersaldo för de
+        // orderrader som finns i ordern
+        await Promise.all(order.order_items.map(async (order_item:
+            Partial<OrderItem>) => {
+                let changedProduct = {
+                    id: order_item.product_id,
+                    name: order_item.name,
+                    stock: order_item.stock - order_item.amount,
+                    api_key: config.api_key,
+            };
+            await products.updateProduct(changedProduct);
+       }));
+
+        // TODO: Ändra status för ordern till packad
+        let changedOrder = {
+            id: order.id,
+            name: order.name,
+            status_id: 200,
+            api_key: config.api_key,
+        };
+
+        await orders.updateOrder(changedOrder);
+    },
+
+    updateOrder: async function updateOrder(order: Partial<Order>) {
+        try {
+            await fetch(`${config.base_url}/orders`, {
+                body: JSON.stringify(order),
+                headers: {
+                    'content-type': 'application/json'
+                },
+                method: 'PUT'
+            });
+            // const result = await response.json();
+            // return result.data;
+        } catch (error) {
+            console.error("could not update order");
+        }
+    },
+    checkProductsAvailability: function checkProductsAvailability(order: Partial<Order>) {
+        let result = true;
+        order.order_items.forEach(item => {
+            if (item.amount > item.stock) {
+                result =  false;
+                return;
+            }
+        });
+        return result;
+    },
+};
+
+export default orders;
---
+++ models/products.ts
@@ -0,0 +1,37 @@
+import config from "../config/config.json";
+import Product from '../titleTextColour';
+import ErrorMessage from '../interfaces/errormessage';
+
+
+const products = {
+    getProducts: async function getProducts() {
+        const response = await fetch(`${config.base_url}/products?api_key=${config.api_key}`);
+        const result = await response.json();
+        return result.data;
+    },
+    updateProduct: async function updateProduct(product: Partial<Product>) {
+        try {
+            await fetch(`${config.base_url}/products`, {
+                body: JSON.stringify(product),
+                headers: {
+                    'content-type': 'application/json'
+                },
+                method: 'PUT'
+            });
+        } catch (error) {
+            console.error("could not update product " + error);
+            return {
+                
+                error: error.message,
+                status: error.status,
+            }
+        }
+    },
+    deleteProduct: async function deleteProduct() {
+        const response = await fetch(`${config.base_url}/products?api_key=${config.api_key}`);
+        const result = await response.json();
+        return result.data;
+    },
+};
+
+export default products;
