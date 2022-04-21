891b6297a82ae9987d42b839478da9c3aa187827
Anya Kovalieva <annakovalieva@gmail.com>
2022-04-21 16:28:09 +0200

    implemented login

--- App.tsx
+++ App.tsx
@@ -3,23 +3,33 @@ import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Home from "./components/Home";
 import Pick from "./components/Pick";
+import Invoices from "./components/Invoices";
 import Deliveries from "./components/Deliveries";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AntDesign } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Base, Typography } from './styles';
-
+import Auth from "./components/auth/Auth";
+import authModel from "./models/auth";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState<Boolean>(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(async () => {
+    setIsLoggedIn(await authModel.loggedIn());
+  }, []);
+
+  console.log("logged in: " + isLoggedIn);
+
   const routeIcons = {
     "Stock": "home",
     "Orders": "profile",
     "Deliveries": "export",
   };
-  const [products, setProducts] = useState([]);
   return (
     <SafeAreaView style={Base.container}>
       <NavigationContainer>
@@ -44,6 +54,15 @@ export default function App() {
           <Tab.Screen name="Deliveries">
             {() => <Deliveries setProducts={setProducts} />}
           </Tab.Screen>
+          {isLoggedIn ?
+            <Tab.Screen name="Invoices">
+              {() => <Invoices setIsLoggedIn={setIsLoggedIn} />}
+            </Tab.Screen> :
+
+            <Tab.Screen name="Log in">
+              {() => <Auth setIsLoggedIn={setIsLoggedIn} />}
+            </Tab.Screen>
+          }
         </Tab.Navigator>
       </NavigationContainer>
       <StatusBar style="auto" />
---
+++ components/auth/AuthFields.tsx
@@ -0,0 +1,49 @@
+import {View, Text, TextInput, Button, Pressable } from "react-native";
+import {Typography, FormStyles, Base } from '../../styles';
+
+
+export default function AuthFields({ auth, setAuth, title, submit, navigation }) {
+    return (
+        <View style={Base.container}>
+            <Text style={Typography.header2}>{title}</Text>
+
+            <Text style={Typography.label}>Email</Text>
+            <TextInput
+                style={FormStyles.input}
+                onChangeText={(content:string) => {
+                    setAuth({ ...auth, email:content })
+                }} 
+                value={auth?.email}
+                keyboardType="email-address"
+                autoCapitalize = "none"
+                autoCorrect={false}
+            />
+
+            <Text style={Typography.label}>Password</Text>
+            <TextInput
+                style={FormStyles.input}
+                onChangeText={(content:string) => {
+                    setAuth({ ...auth, password:content })
+                }}
+                value={auth?.password}
+                secureTextEntry={true}
+                autoCapitalize = "none"
+                autoCorrect = {false}
+            />
+            <Button
+                title={title}
+                onPress={()=> {
+                    submit();
+                }}
+            />
+            {title === "Log in" &&
+                <Button
+                    title="Register"
+                    onPress={() => {
+                        navigation.navigate("Register");
+                    }}
+                />
+            }   
+        </View>
+    );
+};
